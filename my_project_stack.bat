c4ce277caeebbbca285d827f9ad2c62d35861682
Hamania <hamania@gmail.com>
2025-12-12 20:11:11 +0100

    my_project:1.0

--- my_project_stack.bat
+++ my_project_stack.bat
@@ -1,7 +1,7 @@
-docker build -t my_project_api:1.0 ./my_project_api
+@REM docker build -t my_project_api:1.0 ./my_project_api
 @REM docker run -p 8001:8000 my_project_api:1.0
 
-docker build -t my_project_vue:1.0 ./my_project_vue
+@REM docker build -t my_project_vue:1.0 ./my_project_vue
 @REM docker run -p 8080:80 my_vue_project:1.0
 
 @REM docker stack rm my_project
