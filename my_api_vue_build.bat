8835c8fe73485e3adcaeedb8eb5322ed3fc67311
Hamania <hamania@gmail.com>
2025-12-12 18:30:26 +0100

    my_project_***

--- my_api_vue_build.bat
+++
@@ -1,1 +0,0 @@
-docker build -t my_api_project:1.0 ./my_api_project
--- my_postgresql/postgres_16.bat
+++
@@ -1,3 +0,0 @@
-@REM docker pull postgres:latest
-
-docker run --name postgres-container -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
